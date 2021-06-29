a6dbd1a0c435f2b425fae3d6dc219b92842ba812
yamaday0u <35hallelujah00@gmail.com>
2021-06-30 01:18:18 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'spa_roots/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope 'spa' do
     get '*path', to: 'spa_roots#show'
   end
